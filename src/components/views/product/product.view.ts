21eb5e9a13b44b1826af9f2bcb61236bba0b0926
julia <yusiagerachka@gmail.com>
2022-12-22 22:09:16 +0300

    feat: prodView: add change btn text

--- src/components/views/product/product.view.ts
+++ src/components/views/product/product.view.ts
@@ -3,29 +3,55 @@ import ProductInterface from '../../../global/interfaces/product.interface';
 import prodViewTemplate from '../product/product.html';
 
 export default class ProductView {
-
+  private isAddedToBin = false;
   private fillTemplate(product: ProductInterface): string {
     let template = prodViewTemplate;
     const entries = Object.entries(product);
     for (const [key, value] of entries) {
-      template = template.replace(`{{${key}}}`, value.toString());
+      template = template.replace(new RegExp(`{{${key}}}`, 'g'), value.toString());
     }
     return template;
   }
 
-  loadContent(rootElem: string, product: ProductInterface): void {
+  private deleteFromCart(productId: number): void {
+    window.dispatchEvent(new CustomEvent("bindeleted", {
+      detail: { productId: productId }
+    }));
+    this.isAddedToBin = false;
+    this.setBinBtnText(this.isAddedToBin);
+  }
+
+  private addToCart(productId: number): void {
+    window.dispatchEvent(new CustomEvent("binadded", {
+      detail: { productId: productId }
+    }));
+    this.isAddedToBin = true;
+    this.setBinBtnText(this.isAddedToBin);
+  }
+
+  private setBinBtnText(isAddedToBin: boolean): HTMLButtonElement {
+    const buttonAdd = document.getElementById("prod-add-cart") as HTMLButtonElement;
+    buttonAdd.textContent = isAddedToBin ? "Delete from cart" : "Add to cart";
+    return buttonAdd;
+  }
+
+  loadContent(rootElem: string, product: ProductInterface, isAddedToBin: boolean): void {
+    this.isAddedToBin = isAddedToBin;
     const rootElemHtml = document.getElementById(rootElem) as HTMLElement;
     rootElemHtml.innerHTML = "";
 
     rootElemHtml.innerHTML = this.fillTemplate(product);
-    const buttonBuy = document.getElementById("prod-buy-btn");
-    const buttonAdd = document.getElementById("prod-add-cart");
-    
-    buttonBuy?.addEventListener('click', () => {console.log("prod-buy-now") })
-    buttonAdd?.addEventListener('click', () => {  console.log("prod-add-cart");})
-  }
+    const buttonBuy = document.getElementById("prod-buy-btn") as HTMLButtonElement;
+    const buttonAdd = this.setBinBtnText(isAddedToBin);
 
-  addToCart(): void {
-    console.log("title");
+    buttonBuy?.addEventListener('click', () => { console.log("prod-buy-now") })
+    buttonAdd?.addEventListener('click', () => {
+      if (this.isAddedToBin) {
+        this.deleteFromCart(product.id);
+      }
+      else {
+        this.addToCart(product.id);
+      }
+    });
   }
 }
