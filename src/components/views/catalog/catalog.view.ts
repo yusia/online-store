356a3d4cb49d4f1b9d8d654629caa6452b7b40f1
ChristinaKarlovich <christina.karlovich@gmail.com>
2022-12-25 01:48:25 +0300

    feat: add product to cart from catalog

--- src/components/views/catalog/catalog.view.ts
+++ src/components/views/catalog/catalog.view.ts
@@ -3,12 +3,10 @@ import ProductInterface from '../../../global/interfaces/product.interface';
 import FilterParametersInterface from '../../../global/interfaces/filterParameters.interface';
 
 export default class CatalogView {
-  loadContent(
+  private fillProductCatalog(
     rootElem: string,
-    products: Array<ProductInterface>,
-    filterParams: FilterParametersInterface
-  ): void {
-    console.log(filterParams);
+    products: Array<{ product: ProductInterface; isAddedToBin: boolean }>
+  ) {
     const rootElemHtml = document.getElementById(rootElem) as HTMLElement;
     rootElemHtml.innerHTML = catalog;
 
@@ -18,25 +16,85 @@ export default class CatalogView {
 
     const containerProductList = rootElemHtml.querySelector('#product-list');
 
-    products.forEach((product) => {
+    products.forEach((productItem) => {
       const cardElement = templateCard?.content.cloneNode(true) as HTMLElement;
+      const cardDiv = cardElement.querySelector('.card') as HTMLElement;
+      cardDiv.id = `card${productItem.product.id}`;
+
+      function setBinBtnText(isAddedToBin: boolean): HTMLButtonElement {
+        const buttonAdd = cardDiv.querySelector(
+          '.prod-add-cart'
+        ) as HTMLButtonElement;
+        buttonAdd.textContent = isAddedToBin
+          ? 'Delete from cart'
+          : 'Add to cart';
+        return buttonAdd;
+      }
+
+      function deleteFromCart(productItem: {
+        product: ProductInterface;
+        isAddedToBin: boolean;
+      }): void {
+        window.dispatchEvent(
+          new CustomEvent('bindeleted', {
+            detail: { productId: productItem.product.id },
+          })
+        );
+        productItem.isAddedToBin = false;
+        setBinBtnText(productItem.isAddedToBin);
+      }
+
+      function addToCart(productItem: {
+        product: ProductInterface;
+        isAddedToBin: boolean;
+      }): void {
+        window.dispatchEvent(
+          new CustomEvent('binadded', {
+            detail: { productId: productItem.product.id },
+          })
+        );
+        productItem.isAddedToBin = true;
+        setBinBtnText(productItem.isAddedToBin);
+      }
 
       const name = cardElement.querySelector('.card-title') as HTMLElement;
-      name.innerHTML = product.title;
+      name.innerHTML = productItem.product.title;
 
       const img = cardElement.querySelector('.thumbnail') as HTMLImageElement;
-      img.src = product.thumbnail;
+      img.src = productItem.product.thumbnail;
 
       const description = cardElement.querySelector(
         '.card-text'
       ) as HTMLElement;
-      description.innerHTML = product.description;
+      description.innerHTML = productItem.product.description;
 
       const navLink = cardElement.querySelector('.nav-link') as HTMLLinkElement;
-      navLink.href = `#product?prodId=${product.id}`;
+      navLink.href = `#product?prodId=${productItem.product.id}`;
+
+      const addToBinBtn = setBinBtnText(productItem.isAddedToBin);
+      addToBinBtn?.addEventListener('click', () => {
+        if (productItem.isAddedToBin) {
+          deleteFromCart(productItem);
+        } else {
+          addToCart(productItem);
+        }
+      });
 
       containerProductList?.append(cardElement);
     });
+  }
+
+  loadContent(
+    rootElem: string,
+    products: Array<{ product: ProductInterface; isAddedToBin: boolean }>,
+    filterParams: FilterParametersInterface
+  ): void {
+    console.log(filterParams);
+
+    const rootElemHtml = document.getElementById(rootElem) as HTMLElement;
+    rootElemHtml.innerHTML = catalog;
+
+    this.fillProductCatalog(rootElem, products);
 
     const templateCategoryBrand = rootElemHtml.querySelector(
       '#category-brand-template'
