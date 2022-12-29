2b16db00a6f18ead589b8e3999919885203115d0
ChristinaKarlovich <christina.karlovich@gmail.com>
2022-12-30 01:52:25 +0300

    fix: wrong url after update, if original url doesnt have question sighn

--- src/components/views/catalog/catalog.controller.ts
+++ src/components/views/catalog/catalog.controller.ts
@@ -48,7 +48,7 @@ export default class CatalogController implements ControllerInterface {
   updateUrl(parameter: string, value: string) {
     if (parameter == 'viewtype') {
       const searchParams = new URLSearchParams(
-        `?${window.location.href.split('?')[1]}`
+        `?${window.location.href.split('?')[1] || ''}`
       );
       searchParams.set('view', value);
 
